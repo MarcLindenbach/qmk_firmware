30f9cc3fb94a28133c4b6569eb01e0595415be3c
Marc Lindenbach <marc.lindenbach@gmail.com>
2020-02-09 14:23:19 -0700

    Added my layout for the BM16A macropad

---
+++ keyboards/bm16a/keymaps/marc/keymap.c
@@ -0,0 +1,67 @@
+/* Copyright 2019
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+enum layers {
+  _BASE = 0,
+  _FN,
+};
+
+// Defines the keycodes used by our macros in process_record_user
+enum custom_keycodes {
+  QMKBEST = SAFE_RANGE,
+  QMKURL
+};
+
+#define FN_SLS LT(_FN, KC_PSLS)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [_BASE] = LAYOUT_ortho_4x4(
+    KC_P7,    KC_P8,    KC_P9,    KC_PAST, \
+    KC_P4,    KC_P5,    KC_P6,    KC_PMNS, \
+    KC_P1,    KC_P2,    KC_P3,    KC_PPLS, \
+    KC_P0,    FN_SLS,   KC_PDOT,  KC_PENT  \
+  ),
+  [_FN] = LAYOUT_ortho_4x4(
+    _______,  KC_VOLU,  _______,  RESET,   \
+    KC_MPRV,  KC_MPLY,  KC_MNXT,  _______, \
+    _______,  KC_VOLD,  _______,  _______, \
+    _______,  _______,  _______,  KC_NLCK  \
+  ),
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case QMKBEST:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING("QMK is the best thing ever!");
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case QMKURL:
+      if (record->event.pressed) {
+        // when keycode QMKURL is pressed
+        SEND_STRING("https://qmk.fm/" SS_TAP(X_ENTER));
+      } else {
+        // when keycode QMKURL is released
+      }
+      break;
+  }
+  return true;
+}
