07ac1cc9276f8793c04ffa6dffd9051edaa45603
Marc Lindenbach <marc.lindenbach@gmail.com>
2020-02-12 08:23:20 -0700

    Added back ctrl to right hand side of keyboard

--- keyboards/primekb/prime_e/keymaps/marc/keymap.c
+++ keyboards/primekb/prime_e/keymaps/marc/keymap.c
@@ -18,12 +18,13 @@
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 #define KC_CTAB MT(MOD_LCTL, KC_TAB)
+#define KC_CQOT MT(MOD_RCTL, KC_TAB)
 #define KC_SHNT MT(MOD_RSFT, KC_ENT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LAYOUT(
 		KC_GRV,   KC_Q,     KC_W,     KC_E,   KC_R,    KC_T,          KC_Y,   KC_U,     KC_I, KC_O,    KC_P,    KC_MINS, KC_BSLS,
-		KC_CTAB,  KC_A,     KC_S,     KC_D,   KC_F,    KC_G,          KC_H,   KC_J,     KC_K, KC_L,    KC_SCLN, KC_QUOT,
+		KC_CTAB,  KC_A,     KC_S,     KC_D,   KC_F,    KC_G,          KC_H,   KC_J,     KC_K, KC_L,    KC_SCLN, KC_CQOT,
 		KC_LSFT,  KC_Z,     KC_X,     KC_C,   KC_V,    KC_B,          MO(3),  KC_N,     KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_SHNT,
 		KC_LCTL,  KC_LALT,                    KC_LGUI, LT(1, KC_BSPC),LT(2, KC_SPC),    KC_RGUI,                KC_RALT, KC_RCTL
     ),
