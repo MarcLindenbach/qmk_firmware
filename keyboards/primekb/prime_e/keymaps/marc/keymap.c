f22974dedb9b7efc4b2a7bc5d04c802133f1842e
Marc Lindenbach <marc.lindenbach@gmail.com>
2020-03-12 09:46:55 -0600

    Moved quote/enter around for more 40 like layout

--- keyboards/primekb/prime_e/keymaps/marc/keymap.c
+++ keyboards/primekb/prime_e/keymaps/marc/keymap.c
@@ -18,26 +18,26 @@
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 #define KC_CTAB MT(MOD_LCTL, KC_TAB)
-#define KC_SHNT MT(MOD_RSFT, KC_ENT)
+/* #define KC_SHNT MT(MOD_RSFT, KC_ENT) */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LAYOUT(
 		KC_GRV,   KC_Q,     KC_W,     KC_E,   KC_R,    KC_T,          KC_Y,   KC_U,     KC_I, KC_O,    KC_P,    KC_MINS, KC_BSLS,
-		KC_CTAB,  KC_A,     KC_S,     KC_D,   KC_F,    KC_G,          KC_H,   KC_J,     KC_K, KC_L,    KC_SCLN, KC_QUOT,
-		KC_LSFT,  KC_Z,     KC_X,     KC_C,   KC_V,    KC_B,          MO(3),  KC_N,     KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_SHNT,
+		KC_CTAB,  KC_A,     KC_S,     KC_D,   KC_F,    KC_G,          KC_H,   KC_J,     KC_K, KC_L,    KC_SCLN, KC_ENT,
+		KC_LSFT,  KC_Z,     KC_X,     KC_C,   KC_V,    KC_B,          MO(3),  KC_N,     KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
 		KC_LCTL,  KC_LALT,                    KC_LGUI, LT(1, KC_BSPC),LT(2, KC_SPC),    KC_RGUI,                KC_RALT, KC_RCTL
     ),
 
     LAYOUT(
     KC_ESC,    KC_EXLM,   KC_AT,     KC_HASH,   KC_DLR ,    KC_PERC,          KC_CIRC,   KC_AMPR,    KC_ASTR,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
-    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,    XXXXXXX,          XXXXXXX,   KC_LCBR,    KC_EQL ,   KC_RCBR,   XXXXXXX,   XXXXXXX,
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,    XXXXXXX,          XXXXXXX,   KC_LCBR,    KC_EQL ,   KC_RCBR,   XXXXXXX,   KC_QUOT,
     KC_LPRN,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,    XXXXXXX,          XXXXXXX,   XXXXXXX,    KC_LBRC,   KC_PLUS,   KC_RBRC,   XXXXXXX,   KC_RPRN,
 		_______,   _______,                         _______,    _______,	        _______,   _______,                                     _______,   _______
     ),
 
     LAYOUT(
 		KC_ESC,   KC_1,      KC_2,      KC_3,      KC_4,       KC_5,             KC_6,      KC_7,     KC_8,    KC_9,    KC_0,      XXXXXXX,  XXXXXXX,
-		KC_DEL,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,    XXXXXXX,          KC_LEFT,   KC_DOWN,  KC_UP,   KC_RGHT, XXXXXXX,   XXXXXXX,
+		KC_DEL,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,    XXXXXXX,          KC_LEFT,   KC_DOWN,  KC_UP,   KC_RGHT, XXXXXXX,   KC_DQT,
 		XXXXXXX,  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,    XXXXXXX,          KC_HOME,   KC_PGDN,  KC_PGUP, KC_END,  XXXXXXX,   XXXXXXX,  XXXXXXX,
 		_______,  _______,                         _______,    _______,	         _______,   _______,                               _______,  _______
     ),
