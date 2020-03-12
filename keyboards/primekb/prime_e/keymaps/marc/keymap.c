a22621530143edee2b3728ee136fecd9671770e2
Marc Lindenbach <marc.lindenbach@gmail.com>
2020-03-12 10:29:56 -0600

    Moved delete

--- keyboards/primekb/prime_e/keymaps/marc/keymap.c
+++ keyboards/primekb/prime_e/keymaps/marc/keymap.c
@@ -39,9 +39,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   LAYOUT(
 		KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,        KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     XXXXXXX,  XXXXXXX,
-		KC_DEL,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,     KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  XXXXXXX,  KC_DQT,
+		XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,     KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  XXXXXXX,  KC_DQT,
 		XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,     KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   XXXXXXX,  XXXXXXX,  XXXXXXX,
-		_______,  _______,                      _______,  _______,     _______,  _______,                                _______,  _______
+		_______,  _______,                      _______,  KC_DEL,      _______,  _______,                                _______,  _______
   ),
 
   LAYOUT(
