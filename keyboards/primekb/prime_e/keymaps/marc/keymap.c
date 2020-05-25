54cf1e5ec7b0385250acd91f7d37879b03bc8678
Marc Lindenbach <marc.lindenbach@gmail.com>
2020-05-25 09:17:31 -0600

    Updated prime layout with new top right bsps

--- keyboards/primekb/prime_e/keymaps/marc/keymap.c
+++ keyboards/primekb/prime_e/keymaps/marc/keymap.c
@@ -18,27 +18,27 @@
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 #define KC_CTAB MT(MOD_LCTL, KC_TAB)
-#define BCK_LWR LT(1, KC_BSPC)
+#define LWR MO(1)
 #define SPC_RSE LT(2, KC_SPC)
 #define KC_SHNT MT(MOD_RSFT, KC_ENT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   LAYOUT(
-		KC_GRV,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,        KC_Y,     KC_U,    KC_I,      KC_O,     KC_P,     KC_MINS,  KC_BSLS,
+		KC_GRV,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,        KC_Y,     KC_U,    KC_I,      KC_O,     KC_P,     KC_MINS,  KC_BSPC,
 		KC_CTAB,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,        KC_H,     KC_J,    KC_K,      KC_L,     KC_SCLN,  KC_QUOT,
 		KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,        MO(3),    KC_N,    KC_M,      KC_COMM,  KC_DOT,   KC_SLSH,  KC_SHNT,
-		KC_LCTL,  KC_LALT,                      KC_LGUI,  BCK_LWR,     SPC_RSE,  KC_RGUI,                                KC_RALT,  KC_RCTL
+		KC_LCTL,  KC_LALT,                      KC_LGUI,  LWR,         SPC_RSE,  KC_RGUI,                                KC_RALT,  KC_RCTL
   ),
 
   LAYOUT(
-    KC_ESC,   KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR ,  KC_PERC,     KC_CIRC,  KC_AMPR,  KC_ASTR,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    KC_ESC,   KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR ,  KC_PERC,     KC_CIRC,  KC_AMPR,  KC_ASTR,  XXXXXXX,  XXXXXXX,  KC_BSLS,  XXXXXXX,
     _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,     XXXXXXX,  KC_LCBR,  KC_EQL ,  KC_RCBR,  XXXXXXX,  _______,
     KC_LPRN,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,     XXXXXXX,  XXXXXXX,  KC_LBRC,  KC_PLUS,  KC_RBRC,  XXXXXXX,  KC_RPRN,
 		_______,  _______,                      _______,  _______,     _______,  _______,                                _______,  _______
   ),
 
   LAYOUT(
-		KC_DEL,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,        KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     XXXXXXX,  XXXXXXX,
+		KC_DEL,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,        KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_PIPE,  XXXXXXX,
 		_______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,     KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  XXXXXXX,  _______,
 		XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,     KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   XXXXXXX,  XXXXXXX,  XXXXXXX,
 		_______,  _______,                      _______,  _______,     _______,  _______,                                _______,  _______
