daddfe50e0b87e3d9def933840ecdb9484dae735
Marc Lindenbach <marc.lindenbach@gmail.com>
2020-08-21 10:16:04 -0600

    Swapped mins layers

--- keyboards/thevankeyboards/minivan/keymaps/marc/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/marc/keymap.c
@@ -14,13 +14,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL, KC_LALT, KC_LGUI,         MO(1),        LT(2, KC_SPC),         KC_RGUI, KC_RALT, KC_RCTL
   ),
   [1] = LAYOUT( /* LAYER 1 */
-    KC_ESC , KC_EXLM, KC_AT,   KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_PIPE, KC_BSLS, KC_MINS,
+    KC_ESC , KC_EXLM, KC_AT,   KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_PIPE, KC_BSLS, KC_UNDS,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR, KC_EQL , KC_RCBR, XXXXXXX, XXXXXXX,
     KC_LPRN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_PLUS, KC_RBRC, XXXXXXX, KC_RPRN,
     _______, _______, _______,                   _______, _______,          _______, _______, _______
   ),
   [2] = LAYOUT( /* LAYER 2 */
-    KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_UNDS,
+    KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS,
     KC_DEL , XXXXXXX, KC_LCBR, KC_EQL , KC_RCBR, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX, XXXXXXX,
     _______, XXXXXXX, KC_LBRC, KC_PLUS, KC_RBRC, XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END , XXXXXXX, _______,
     RESET  , _______, _______,                   _______, _______,          _______, _______, _______
