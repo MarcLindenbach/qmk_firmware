5b7095a3bff748d824ae62b7451a879bcab03d17
Marc Lindenbach <marc.lindenbach@gmail.com>
2020-11-09 11:10:35 -0700

    Moved minus/underscore and added escape

--- keyboards/minidox/keymaps/marc/keymap.c
+++ keyboards/minidox/keymaps/marc/keymap.c
@@ -23,20 +23,20 @@ const uint16_t  PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
   CTRL_A,  KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    CMD_J,   KC_K,    KC_L,    CTRL_SC,
   Z_SFT,   KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  SH_ENT,
-                    KC_LALT, KC_LGUI, LWR,          SPC_RSE, KC_RGUI, KC_RALT
+                    KC_LALT, KC_LGUI, LWR,          SPC_RSE, KC_RGUI, KC_ESC
 ),
 
 [_RAISE] = LAYOUT(
   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
   KC_BSLS, KC_LCBR, KC_EQL,  KC_RCBR, KC_GRV,       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT,
-  KC_PIPE, KC_LBRC, KC_PLUS, KC_RBRC, KC_TILD,      XXXXXXX, XXXXXXX, XXXXXXX, KC_QUES, KC_SLSH,
+  KC_PIPE, KC_LBRC, KC_PLUS, KC_RBRC, KC_TILD,      _______, _______, _______, KC_QUES, KC_SLSH,
                     _______, _______, _______,      _______, _______, _______
 ),
 
 [_LOWER] = LAYOUT(
   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, _______, KC_DEL,
-  KC_TAB,  _______, _______, _______, _______,      _______, _______, _______, KC_MINS, KC_DQT,
-  KC_LPRN, _______, _______, _______, _______,      _______, _______, _______, KC_UNDS, KC_RPRN,
+  KC_TAB,  _______, _______, _______, _______,      KC_MINS, _______, _______, _______, KC_DQT,
+  KC_LPRN, _______, _______, _______, _______,      KC_UNDS, _______, _______, _______, KC_RPRN,
                     _______, _______, _______,      _______, _______, _______
 ),
 };
