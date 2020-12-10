f1882cb64908274c7bda6904dc158ed17f78bef1
Marc Lindenbach <marc.lindenbach@gmail.com>
2020-12-10 09:06:39 -0700

    Set up per key tapping and added gui j

--- keyboards/thevankeyboards/minivan/keymaps/marc/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/marc/keymap.c
@@ -5,13 +5,14 @@ extern keymap_config_t keymap_config;
 #define XXXXXXX KC_NO
 #define KC_CTAB MT(MOD_LCTL,KC_TAB)
 #define KC_SHNT MT(MOD_RSFT, KC_ENT)
+#define GUI_J MT(MOD_LGUI, KC_J)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT( /* Qwerty */
     KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_CTAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_CTAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    GUI_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SHNT,
-    KC_LCTL, KC_LALT, KC_LGUI,         MO(1),        LT(2, KC_SPC),         KC_RGUI, KC_RALT, KC_RCTL
+    KC_LALT, XXXXXXX, KC_LGUI,         MO(1),        LT(2, KC_SPC),         KC_RGUI, KC_RALT, KC_RCTL
   ),
   [1] = LAYOUT( /* LAYER 1 */
     KC_ESC , KC_EXLM, KC_AT,   KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______, _______, KC_UNDS,
@@ -26,3 +27,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RESET  , _______, _______,                   _______, _______,          _______, _______, _______
   ),
 };
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case KC_SHNT:
+      return 170;
+    default:
+      return 180;
+  }
+}
