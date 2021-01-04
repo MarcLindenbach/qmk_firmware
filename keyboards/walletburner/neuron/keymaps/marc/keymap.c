65103dafb9c32ff3dce1d4226fbfb701dd1c2ab1
Marc Lindenbach <marc.lindenbach@gmail.com>
2021-01-04 08:08:09 -0700

    Added gui j and custom tapping term to neuron

--- keyboards/walletburner/neuron/keymaps/marc/keymap.c
+++ keyboards/walletburner/neuron/keymaps/marc/keymap.c
@@ -6,11 +6,12 @@
 #define KC_CTAB MT(MOD_LCTL, KC_TAB)
 #define SPC_RSE LT(2, KC_SPC)
 #define KC_SHNT MT(MOD_RSFT, KC_ENT)
+#define GUI_J MT(MOD_LGUI, KC_J)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
 		KC_GRV,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,    KC_I,      KC_O,     KC_P,     KC_BSPC,
-		KC_CTAB,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,    KC_K,      KC_L,     KC_SCLN,
+		KC_CTAB,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     GUI_J,   KC_K,      KC_L,     KC_SCLN,
 		KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,    KC_COMM,   KC_DOT,   KC_SHNT,
 		          KC_LALT,  KC_LGUI,            MO(1),    SPC_RSE,  SPC_RSE,           KC_RGUI,   KC_RCTL
   ),
@@ -29,4 +30,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		          _______,  _______,            _______,  _______,  _______,            _______,  _______
   ),
 };
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case KC_SHNT:
+      return 150;
+    default:
+      return 180;
+  }
+}
 
