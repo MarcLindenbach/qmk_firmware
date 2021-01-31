f5c589fdad986e8c319aa536cd90e5870086be3f
Marc Lindenbach <marc.lindenbach@gmail.com>
2021-01-31 08:55:50 -0700

    Updated Amanda's layout to MIT

---
+++ keyboards/planck/keymaps/amanda/config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+#define PREVENT_STUCK_MODIFIERS
+#define PERMISSIVE_HOLD
+#define TAPPING_TERM 180
+
+#endif
--- keyboards/planck/keymaps/amanda/keymap.c
+++ keyboards/planck/keymaps/amanda/keymap.c
@@ -31,8 +31,8 @@ enum planck_layers {
   _RAISE,
 };
 
-#define LOWER LT(_LOWER, KC_ENT)
-#define RAISE LT(_RAISE, KC_SPC)
+#define LOWER MO(_LOWER)
+#define RAISE MO(_RAISE)
 #define CTAB MT(MOD_LCTL, KC_TAB)
 #define SENT MT(MOD_RSFT, KC_ENT)
 
@@ -41,8 +41,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_planck_grid(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     CTAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_RSFT,
-    _______, KC_LCTL, KC_LGUI, KC_LALT, LOWER,  LOWER,    RAISE,   RAISE,   KC_RCTL, KC_LEFT, KC_DOWN, KC_RIGHT
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   SENT,
+    _______, KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RCTL, KC_LEFT, KC_DOWN, KC_RIGHT
 ),
 
 [_LOWER] = LAYOUT_planck_grid(
